80139e53c170b4a6b0f174801812b2a18235797a
Ziang Guo <123800660+ziangkuo@users.noreply.github.com>
2024-03-27 21:41:47 +0100

    login with google

--- src/main.js
+++ src/main.js
@@ -13,20 +13,39 @@ Vue.config.productionTip = false;
 // Vue.use(Button)
 Vue.use(ElementUI);
 
-//添加全局前置导航守卫
+// //添加全局前置导航守卫
+// router.beforeEach((to, from, next) => {
+//    //判断 token 是否存在
+//   const token = Cookie.get('token');
+//   //token 不存在,说明没有登录，跳转到登录页面
+//   if (!token && to.name !== 'login') {
+//     next({ name: 'login' });
+//   } else if (token && to.name === 'login') {
+//     //token存在，说明已经登录，跳转到首页
+//     next({ name: 'home' });
+//   } else {
+//     next();
+//   }
+// });
+
 router.beforeEach((to, from, next) => {
-   //判断 token 是否存在
-  const token = Cookie.get('token');
-  //token 不存在,说明没有登录，跳转到登录页面
-  if (!token && to.name !== 'login') {
-    next({ name: 'login' });
-  } else if (token && to.name === 'login') {
-    //token存在，说明已经登录，跳转到首页
-    next({ name: 'home' });
+  const isSignedIn = store.state.tab.isSignedIn;
+  // 如果用户尝试访问需要登录的页面
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // 用户未登录，跳转到登录页面
+    if (!isSignedIn) {
+      next({ name: "login" });
+    } else {
+      next();
+    }
+  } else if (isSignedIn && to.name === "login") {
+    // 用户已经登录，但尝试访问登录页面，跳转到首页
+    next({ name: "home" });
   } else {
     next();
   }
 });
+
 
 new Vue({
   router,
--- src/store/tab.js
+++ src/store/tab.js
@@ -1,11 +1,15 @@
 export default{
     state:{
-        isCollapse:false//控制菜单的展开和收起
+        isCollapse:false,//控制菜单的展开和收起
+        isSignedIn: false
     },
     mutations:{
         //修改菜单的展开和收起的方法
         collapseMenu(state){
             state.isCollapse = !state.isCollapse
+        },
+        setSignedIn(state, isSignedIn){
+            state.isSignedIn = isSignedIn
         }
     }
 }
